56cf2349a9e5220437060b54549d371d7cbf7e4a
Travis <tervisone@gmail.com>
2021-06-17 19:07:41 -0400

    Intial commit

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -123,7 +123,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      html = insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+	  finalHtml += html;
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
