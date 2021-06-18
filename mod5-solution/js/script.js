47221946b5cddcea1b72f1630e201271b74e17a9
Travis <tervisone@gmail.com>
2021-06-17 20:34:59 -0400

    Intial commit

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -102,6 +102,8 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
 	  var randomCategory = chooseRandomCategory(categories);
       var chosenCategoryShortName = randomCategory["short_name"];
+	  
+	  console.log(homeHtml);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -115,13 +117,13 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(html, "randomCategoryShortName", chosenCategoryShortName);
-	  console.log(homeHtmlToInsertIntoMainPage);
+      // var homeHtmlToInsertIntoMainPage = 
+	  
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+	  
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
